b22ada13b6fcf4dc1c2e6314cc433d59fca834b3
Sharon Xiao <sharonxi@usc.edu>
2019-01-13 21:50:52 -0800

    testing out committing multiple files with git

---
+++ random1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "first testing for hw1 q2c" << std::endl;
+}
---
+++ random2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "second testing for hw1 q2c" << std::endl;
+}
